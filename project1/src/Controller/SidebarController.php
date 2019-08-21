49144321fb01f54a9f61ce766c26e37bb63cdb13
Łukasz Brzozowski <lukasz.brzozowski@i-bs.pl>
2019-08-21 15:55:42 +0200

    porządki w css menu

--- project1/src/Controller/SidebarController.php
+++ project1/src/Controller/SidebarController.php
@@ -7,14 +7,5 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SidebarController extends AbstractController
 {
-    /**
-     * @Route("/default", name="default")
-     */
-    public function index()
-    {
-//        return $this->render('default/index.html.twig', [
-//            'controller_name' => 'DefaultController',
-//        ]);
-        return $this->render('control-sidebar/settings.html.twig', []);
-    }
+
 }
