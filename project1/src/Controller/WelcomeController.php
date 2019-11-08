51fc318302b4c424f3e012307e8fef8ba69920cb
Łukasz Brzozowski <lukasz.brzozowski@i-bs.pl>
2019-11-08 15:24:30 +0100

    Welcome, gradient

---
+++ project1/src/Controller/WelcomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class WelcomeController
+ * @package App\Controller
+ */
+class WelcomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="welcome")
+     */
+    public function index()
+    {
+        return $this->render('welcome/index.html.twig', [
+            'controller_name' => 'WelcomeController',
+        ]);
+    }
+}
